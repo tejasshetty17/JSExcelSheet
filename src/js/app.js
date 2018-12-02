05fc618718ad2d323cdebc7dd3455c73689b6c78
Tejas Shetty <tejas.ganesh@cleartax.in>
2018-12-03 00:20:02 +0530

    minor refactor

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,6 @@
 let data = JSON.parse(localStorage.getItem('data'));
 let rowLength = Number(localStorage.getItem('rows'));
 let columnLength = Number(localStorage.getItem('columns'));
-// if(data){
-//     rowLength = data.length;
-//     columnLength = data.length;
-// }
-// console.log(data);
 let excel = new ExcelSheet('excel-sheet', data, rowLength, columnLength);
 excel.render();
 excel.initEvent();
--- src/js/excel.js
+++ src/js/excel.js
@@ -3,12 +3,10 @@ const ADDROWBUTTON = document.getElementById('add-row');
 const ADDCOLUMNBUTTON = document.getElementById('add-column');
 
 function ExcelSheet(element, data, rows, columns) {
-    console.log(rows,columns);
   this.element = element; // elem should be an ID
   this.rows = rows || 90;
   this.columns = columns || 20;
   this.data = data || [];
-  console.log(this.rows,this.columns);
   this.initializeData = () =>{
     if(data){
         return;
@@ -68,7 +66,6 @@ ExcelSheet.prototype.render = function(){
     for(let i = 0; i <= this.rows; i++){
         let row = TABLE.insertRow(i);
         for(let j = 0; j <= this.columns; j++){
-            console.log(excel.data[i][j]);
             if(i===0 && (j > 0)){
                 row.insertCell(j).innerHTML = `<input style="width:88px;" class="excel-column-header" type="text" disabled data-row=${i} data-column=${j} value="${String.fromCharCode(j + 64)}"/><input id="${j}" type="button" class="btn btn-sec delete-column" value="Delete">`;
             } else if (j === 0 && (i > 0)) {
